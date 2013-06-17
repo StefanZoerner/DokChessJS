0aab117847c798d4e28aca751bb244fbb9e92fe6
StefanZoerner <stefan@labeo.de>
2013-06-17 21:20:16 +0200

    Header-Files geaendert

--- Iter1/elementeTest.js
+++ Iter1/elementeTest.js
@@ -1,9 +1,19 @@
-/**
- * Created with JetBrains WebStorm.
- * User: StefanZ
- * Date: 10.06.13
- * Time: 22:18
- * To change this template use File | Settings | File Templates.
+/* Copyright 2010, 2011, 2012 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var dokchess = require("./elemente.js");
--- Iter1/xboard.js
+++ Iter1/xboard.js
@@ -1,21 +1,31 @@
-/**
- * Created with JetBrains WebStorm.
- * User: StefanZ
- * Date: 11.06.13
- * Time: 23:10
- * To change this template use File | Settings | File Templates.
+/* Copyright 2010, 2011, 2012 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-var schachdomaene = require("./elemente.js");
+var elemente = require("./elemente.js");
 
-var Stellung = schachdomaene.Stellung;
-var Zug = schachdomaene.Zug;
-var FigurenArt = schachdomaene.FigurenArt;
-var Feld = schachdomaene.Feld;
-var Farbe = schachdomaene.Farbe;
+var Stellung = elemente.Stellung;
+var Zug = elemente.Zug;
+var FigurenArt = elemente.FigurenArt;
+var Feld = elemente.Feld;
+var Farbe = elemente.Farbe;
 
 var stellung = new Stellung();
 
