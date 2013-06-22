f726b73283488150db72d47714f93a8cecef1a4b
StefanZoerner <stefan@labeo.de>
2013-06-22 16:23:12 +0200

    Fehlendes "use strict"; eingefuegt

--- Iter3/tools.js
+++ Iter3/tools.js
@@ -1,10 +1,22 @@
-/**
- * Created with JetBrains WebStorm.
- * User: StefanZ
- * Date: 01.10.12
- * Time: 21:41
- * To change this template use File | Settings | File Templates.
+/* Copyright 2013 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+"use strict";
 
 var contains = function (array, obj) {
     var i = 0;
